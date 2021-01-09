0585a8cea4eee3604706f619025a6227f5221476
Léa DELANNAY <lea.delannay@pole-emploi.fr>
2021-01-09 15:39:19 +0100

    chore(tests): add profiles, h2 and tests configuration

--- src/test/java/com/epsi/ic_automobile/IcAutomobileApplicationTests.java
+++ src/test/java/com/epsi/ic_automobile/IcAutomobileApplicationTests.java
@@ -2,8 +2,10 @@ package com.epsi.ic_automobile;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IcAutomobileApplicationTests {
 
 	@Test
