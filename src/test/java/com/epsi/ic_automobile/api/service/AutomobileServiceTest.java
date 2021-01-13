4a205b0fcdce49a5eef5446ec8490f41d933851f
Léa DELANNAY <lea.delannay@pole-emploi.fr>
2021-01-13 12:03:04 +0100

    feat(api) : mise en place test paramétré

--- src/test/java/com/epsi/ic_automobile/api/service/AutomobileServiceTest.java
+++ src/test/java/com/epsi/ic_automobile/api/service/AutomobileServiceTest.java
@@ -6,15 +6,18 @@ import com.epsi.ic_automobile.api.repository.ContratRepository;
 import com.epsi.ic_automobile.model.Client;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @ExtendWith( MockitoExtension.class)
@@ -29,15 +32,25 @@ class AutomobileServiceTest {
     @InjectMocks
     private AutomobileService automobileService;
 
+    private static Stream<Arguments> chargerDonneesTest() {
+        return Stream.of(
+                Arguments.of(1, "client1@ici.fr", "token1"),
+                Arguments.of(2, "client2@ici.fr", "token2"),
+                Arguments.of(3, "client3@ici.fr", "token3"),
+                Arguments.of(4, "client4@ici.fr", "token4"),
+                Arguments.of(5,"client5@ici.fr", "token5")
+        );
+    }
+
     @Test
     public void doitRecupererListeContrats(){
-        //Given
+        //Arrange
         when(contratRepository.findAll()).thenReturn(new ArrayList<>());
 
-        //When
+        //Act
         int resultSize = automobileService.getListeContrats().size();
 
-        //Then
+        //Assert
         assertThat(resultSize).isEqualTo(0);
     }
 
@@ -64,4 +77,23 @@ class AutomobileServiceTest {
         assertThat(idClientFromService).isEqualTo(id_client);
     }
 
+    @ParameterizedTest
+    @MethodSource("chargerDonneesTest")
+    public void doitRecupererLidDuClientParametre(int idClient, String email, String token){
+        //Arrange
+        List<Client> listeClient = new ArrayList<>();
+        Client client = new Client();
+        client.setId_client(idClient);
+        client.setEmail(email);
+        client.setToken(token);
+        listeClient.add(client);
+
+        when(clientRepository.findByEmailAndToken(email, token)).thenReturn(listeClient);
+
+        //Act
+        int idClientFromService = automobileService.getIdClient(email, token);
+
+        //Assert
+        assertThat(idClientFromService).isEqualTo(idClient);
+    }
 }
